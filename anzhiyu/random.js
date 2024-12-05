7a637d193f85c8605080448fd0de3e08c26c1046
ganlili <njganlili@outlook.com>
2024-12-05 14:50:25 +0800

    Site updated: 2024-12-05 14:50:21

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/24/travel/DangLing/","2024/03/23/travel/Emei-Leshan/","2024/10/15/travel/QingDao/","2024/11/27/travel/Reddomain/","2024/09/08/travel/ShenDaGe/","2024/09/18/travel/Siguniang_CTB/","2024/11/23/travel/TheTopOfTheThreeGorges/","2024/09/25/live/food/Food/","2024/10/11/technology/email/Email_OAuth2/","2024/09/11/technology/phone/FCM_record/","2024/12/04/technology/sms/ModelPool/"];function toRandomPost(){
+var posts=["2024/10/24/travel/DangLing/","2024/03/23/travel/Emei-Leshan/","2024/10/15/travel/QingDao/","2024/11/27/travel/Reddomain/","2024/09/08/travel/ShenDaGe/","2024/09/18/travel/Siguniang_CTB/","2024/11/23/travel/TheTopOfTheThreeGorges/","2024/10/11/technology/email/Email_OAuth2/","2024/09/11/technology/phone/FCM_record/","2024/12/04/technology/sms/ModelPool/","2024/09/25/live/food/Food/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
